1bc9d8ee22e2ffb889b0f506381e6d5984f2f53f
TPU2020 <69647477+TPU2020@users.noreply.github.com>
2022-01-13 11:13:54 +0100

    Přidejte soubory projektu.

---
+++ _MainProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KurzProgramovani_CSharp
+{
+    internal class _MainProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
